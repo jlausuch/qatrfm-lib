bb6c9aa52c89bb31cea1d2a6da357d04bedf9377
jlausuch <jlausuch@gmail.com>
2019-02-25 17:53:51 +0100

    Allow running multiple tests with CLI

--- qatrfm/cli.py
+++ qatrfm/cli.py
@@ -13,7 +13,8 @@ from qatrfm.testcase import TrfmTestCase
 
 @click.command()
 @click.option('--test', '-t', required=True,
-              help='Test case name, same name as the Class.')
+              help='Testcase(s) name(s). Single name or a list separated by '
+              'comas of the Class(es) in path to be executed.')
 @click.option('--path', '-p', required=True,
               help='Path of the test file.')
 @click.option('--hdd', '-h', required=True, help='Path to HDD image.')
@@ -27,6 +28,7 @@ from qatrfm.testcase import TrfmTestCase
               help='Use libvirt snapshots for the domains.')
 def run(test, path, hdd, num_domains, cores, ram, snapshots, no_clean):
     logger = QaTrfmLogger.getQatrfmLogger(__name__)
+    test_array = test.split(',')
 
     basedir, filename = os.path.split(path)
     tf_file = None
@@ -41,36 +43,46 @@ def run(test, path, hdd, num_domains, cores, ram, snapshots, no_clean):
     spec = importlib.util.spec_from_file_location(filename, path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
-
-    cls = None
-    for member in inspect.getmembers(module):
-        if member[0] == test:
-            cls = member[1]
-    if (cls is None):
-        sys.exit("There is no Class named '{}' in {}".format(test, path))
-    testcase = cls(env, test)
+    testcases = []
+    for t in test_array:
+        cls = None
+        for member in inspect.getmembers(module):
+            if member[0] == t:
+                cls = member[1]
+        if (cls is None):
+            sys.exit("There is no Class named '{}' in {}".format(t, path))
+        testcases.append(cls(env, t))
 
     logger.info("Test case information:\n"
-                "\t\tTEST      : {}\n"
-                "\t\tDESCRPT.  : {}\n"
-                "\t\tHDD       : {}\n"
-                "\t\tDOMAINS   : {}\n"
-                "\t\tCORES     : {}\n"
-                "\t\tRAM       : {}\n"
-                "\t\tWORKDIR   : {}\n"
-                "\t\tNETWORK   : {}\n"
-                "\t\tCLEAN     : {}\n"
-                "\t\tSNAPSHOTS : {}\n"
-                .format(test, testcase.description, hdd, num_domains,
-                        cores, ram, env.workdir, env.networks[0],
-                        not no_clean, snapshots))
-    exit_status = TrfmTestCase.EX_OK
+                "\t\tTest case(s): {}\n"
+                "\t\tEnvironment:\n"
+                "\t\t  Working dir.: {}\n"
+                "\t\t  Clean       : {}\n"
+                "\t\t  Snapshots   : {}\n"
+                "\t\tDomains:\n"
+                "\t\t  Count  : {}\n"
+                "\t\t  Image  : {}\n"
+                "\t\t  Cores  : {}\n"
+                "\t\t  RAM    : {}\n"
+                "\t\t  Network: {}\n"
+                .format(test, env.workdir, not no_clean, snapshots,
+                        num_domains, hdd, cores, ram, env.networks[0]))
+
+    failed_tests = []
 
     try:
         env.deploy()
-        logger.info("Running test case '{}' ...".format(test))
-        if (testcase.run() != TrfmTestCase.EX_OK):
-            exit_status = TrfmTestCase.EX_RUN_ERROR
+        for t in testcases:
+            logger.info("Running test case '{}' ...".format(t.name))
+
+            exit_code = t.run()
+            if (exit_code == TrfmTestCase.EX_OK):
+                logger.success("The test '{}' finished successfuly".
+                               format(t.name))
+            else:
+                failed_tests.append(t.name)
+                logger.error("The test '{}' finished with error code={}".
+                             format(exit_code))
 
     except Exception as e:
         logger.error("Something went wrong:\n{}".format(e))
@@ -80,8 +92,15 @@ def run(test, path, hdd, num_domains, cores, ram, snapshots, no_clean):
 
     if (not no_clean):
         env.clean()
-    logger.info("The test finished with status={}".format(exit_status))
-    sys.exit(exit_status)
+
+    if (len(testcases) > 1):
+        if (len(failed_tests) > 0):
+            logger.error("The following tests failed: {}".
+                         format(failed_tests))
+            sys.exit(TrfmTestCase.EX_RUN_ERROR)
+        else:
+            logger.success("Overall status = GREEN")
+            sys.exit(TrfmTestCase.EX_OK)
 
 
 if __name__ == '__main__':
--- tests/examples/multi-test/multi_test.py
+++ tests/examples/multi-test/multi_test.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-
-from qatrfm.environment import TerraformEnv
 from qatrfm.testcase import TrfmTestCase
 
 from qatrfm.utils.logger import QaTrfmLogger
@@ -11,12 +8,15 @@ logger = QaTrfmLogger.getQatrfmLogger(__name__)
 
 
 class TestCase1(TrfmTestCase):
+    def __init__(self, env, name):
+        description = "Example Multitest test 1. Ping from VM1 to VM2"
+        super(TestCase1, self).__init__(env, name, description)
+
     def run(self):
-        # test logic here
-        self.logger.info('Running test case {}'.format(self.name))
         vm1 = self.env.domains[0]
         vm2 = self.env.domains[1]
         [retcode, output] = vm1.execute_cmd('ping -c 1 {}'.format(vm2.ip))
+        self.env.reset()
         if (retcode == 0):
             return self.EX_OK
         else:
@@ -24,9 +24,11 @@ class TestCase1(TrfmTestCase):
 
 
 class TestCase2(TrfmTestCase):
+    def __init__(self, env, name):
+        description = "Example Multitest test 2. Ping from VM2 to VM1"
+        super(TestCase2, self).__init__(env, name, description)
+
     def run(self):
-        # test logic here
-        self.logger.info('Run test case {}'.format(self.name))
         vm1 = self.env.domains[0]
         vm2 = self.env.domains[1]
         [retcode, output] = vm2.execute_cmd('ping -c 1 {}'.format(vm1.ip))
@@ -34,34 +36,3 @@ class TestCase2(TrfmTestCase):
             return self.EX_OK
         else:
             return self.EX_RUN_ERROR
-
-
-def main():
-    hdd = ("/var/lib/libvirt/images/my_image.qcow2")
-    env = TerraformEnv(image=hdd, num_domains=2, snapshots=True)
-    env.deploy()
-    exit_status = TrfmTestCase.EX_OK
-
-    try:
-        test1 = TestCase1(env, 'test 1', 'ping from VM1 to VM2')
-        if (test1.run() != TrfmTestCase.EX_OK):
-            exit_status = TrfmTestCase.EX_RUN_ERROR
-
-        env.reset()
-
-        test2 = TestCase2(env, 'test 2', 'ping from VM2 to VM1')
-        if (test2.run() != TrfmTestCase.EX_OK):
-            exit_status = TrfmTestCase.EX_RUN_ERROR
-
-    except Exception as e:
-        logger.error("Something went wrong:\n{}".format(e))
-        env.clean()
-        sys.exit(TrfmTestCase.EX_RUN_ERROR)
-
-    env.clean()
-    logger.info("The test finished with status={}".format(exit_status))
-    sys.exit(exit_status)
-
-
-if __name__ == "__main__":
-    main()
